3b695b530ae1d956ef57a53918abd57ec86c7efb
tom mcandrew <mcandrew@lehigh.edu>
2025-07-25 16:07:32 -0400

    update

--- models/SLURM_runner.sh
+++ models/SLURM_runner.sh
@@ -27,6 +27,7 @@ python -m venv .whoseason
 source .whoseason/bin/activate
 pip install -r requirements.txt
 
+pip uninstall -y jax jaxlib
 pip install --no-binary=:all: jax jaxlib==0.4.38
 
 python ./models/train_past_model_outputs.py --LOCATION ${LOCATION} --SEASON ${SEASON}
